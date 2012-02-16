a8490459df4ce6f30df8b6424e718d62553cade0
Xu, Jiang Yan <me@jxu.me>
2012-02-16 15:06:20 -0500

    Initial commit.

---
+++ src/idigbio/storage/SwiftFileImporter.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2012 Xu, Jiang Yan <me@jxu.me>, University of Florida
+ * This software may be used and distributed according to the terms of the
+ * MIT license: http://www.opensource.org/licenses/mit-license.php 
+ */
+package idigbio.storage;
+
+public class SwiftFileImporter {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
